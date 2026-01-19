e18aa2d82b57a8e6f4b1595a2c371b12e8a49a47
Shaikh Basim <shaikhbasim050@gmail.com>
2026-01-19 14:48:51 -0500

    changes

--- ingest/config.py
+++ ingest/config.py
@@ -1,15 +1,24 @@
-import boto3
-from dotenv import load_dotenv
 import os
+import boto3
 
-load_dotenv()
-
-R2_BUCKET = os.getenv("R2_BUCKET_NAME")
+try:
+    import streamlit as st
+    R2_ACCESS_KEY_ID = st.secrets["R2_ACCESS_KEY_ID"]
+    R2_SECRET_ACCESS_KEY = st.secrets["R2_SECRET_ACCESS_KEY"]
+    R2_ENDPOINT_URL = st.secrets["R2_ENDPOINT_URL"]
+    R2_BUCKET = st.secrets["R2_BUCKET"]
+except:
+    from dotenv import load_dotenv
+    load_dotenv()
+    R2_ACCESS_KEY_ID = os.getenv("R2_ACCESS_KEY_ID")
+    R2_SECRET_ACCESS_KEY = os.getenv("R2_SECRET_ACCESS_KEY")
+    R2_ENDPOINT_URL = os.getenv("R2_ENDPOINT_URL")
+    R2_BUCKET = os.getenv("R2_BUCKET")
 
 def get_s3_client():
     return boto3.client(
         "s3",
-        endpoint_url=os.getenv("R2_ENDPOINT_URL"),
-        aws_access_key_id=os.getenv("R2_ACCESS_KEY_ID"),
-        aws_secret_access_key=os.getenv("R2_SECRET_ACCESS_KEY"),
+        endpoint_url=R2_ENDPOINT_URL,
+        aws_access_key_id=R2_ACCESS_KEY_ID,
+        aws_secret_access_key=R2_SECRET_ACCESS_KEY
     )
