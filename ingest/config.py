b05f5a4ecb9bdb6daad3a2114c3365a5e6566227
Shaikh Basim <shaikhbasim050@gmail.com>
2026-01-20 14:36:30 -0500

    updates

--- ingest/config.py
+++ ingest/config.py
@@ -6,14 +6,14 @@ try:
     R2_ACCESS_KEY_ID = st.secrets["R2_ACCESS_KEY_ID"]
     R2_SECRET_ACCESS_KEY = st.secrets["R2_SECRET_ACCESS_KEY"]
     R2_ENDPOINT_URL = st.secrets["R2_ENDPOINT_URL"]
-    R2_BUCKET = st.secrets["R2_BUCKET"]
+    R2_BUCKET = st.secrets["R2_BUCKET_NAME"]
 except:
     from dotenv import load_dotenv
     load_dotenv()
     R2_ACCESS_KEY_ID = os.getenv("R2_ACCESS_KEY_ID")
     R2_SECRET_ACCESS_KEY = os.getenv("R2_SECRET_ACCESS_KEY")
     R2_ENDPOINT_URL = os.getenv("R2_ENDPOINT_URL")
-    R2_BUCKET = os.getenv("R2_BUCKET")
+    R2_BUCKET = os.getenv("R2_BUCKET_NAME")
 
 def get_s3_client():
     return boto3.client(
